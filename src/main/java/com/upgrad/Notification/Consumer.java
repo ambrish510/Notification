986cf6d21bc59ec388174aeb71e7a18c708447ae
ambrish510 <ambrishpatil510@gmail.com>
2021-11-14 00:08:04 +0530

    Code Formatted & added proper comments

--- src/main/java/com/upgrad/Notification/Consumer.java
+++ src/main/java/com/upgrad/Notification/Consumer.java
@@ -5,7 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.time.Duration;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 import java.util.Set;
 
@@ -13,31 +13,37 @@ public class Consumer {
 
     public static void main(String[] args) {
 
+        /*
+        Setting up the properties for the consumer
+         */
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers","ec2-user@ec2-18-211-51-189.compute-1.amazonaws.com:9092");
-        properties.setProperty("group.id","test");
-        properties.setProperty("enable.auto.commit","true");
-        properties.setProperty("auto.commit.interval.ms","1000");
+        properties.setProperty("bootstrap.servers", "ec2-user@ec2-18-211-51-189.compute-1.amazonaws.com:9092");
+        properties.setProperty("group.id", "test");
+        properties.setProperty("enable.auto.commit", "true");
+        properties.setProperty("auto.commit.interval.ms", "1000");
         properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-
-
-        KafkaConsumer<String,String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
-        kafkaConsumer.subscribe(Arrays.asList("chat_message"));
+        /*
+        Subscribing to the kafka topic & reading message from kafka topic
+         */
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(Collections.singleton("booking_confirmation_message"));
         Set<String> SubScribedTopics = kafkaConsumer.subscription();
-        SubScribedTopics.stream().forEach(System.out::println);
+        SubScribedTopics.forEach(System.out::println);
 
-        try{
-            while(true){
-                ConsumerRecords<String,String> consumerRecords =  kafkaConsumer.poll(Duration.ofMinutes(2));
-                for (ConsumerRecord<String, String> records: consumerRecords) {
-                    System.out.printf("offset = %d, key = %s , vlaue = %s%n", records.offset(),records.key(),records.value());
+        /*
+        Printing the message from kafka topic
+         */
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMinutes(2));
+                for (ConsumerRecord<String, String> records : consumerRecords) {
+                    System.out.printf("offset = %d, key = %s , value = %s%n", records.offset(), records.key(), records.value());
                 }
             }
-        }finally {
+        } finally {
             kafkaConsumer.close();
         }
-
     }
 }
